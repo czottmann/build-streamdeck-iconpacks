9b6fc51b9ff3b06a35cc2bf24fb4c03cdbec700e
Carlo Zottmann <carlo@zottmann.org>
2021-10-03 16:27:23 +0200

    [CHG] Renames variables for readability [CHG] Changes description in manifest

--- build-system-uicons.fish
+++ build-system-uicons.fish
@@ -2,19 +2,28 @@
 
 source ./_functions.fish
 
-set iconpack_name "System UIcons"
-set version_file version.system-uicons.txt
-set iconpack_version (cat $version_file | string trim)
+set sdip_name "System UIcons"
+set sdip_slug (slugify "$sdip_name")
+set dest_folder_name "$sdip_slug.sdIconPack"
+set sdip_version_file version.system-uicons.txt
+set sdip_version (cat $sdip_version_file | string trim)
 
-echo "Preparing to build Stream Deck icon pack \"$iconpack_name\""
+set build_folder (pwd)"/build/$sdip_slug"
+set build_dest_folder "$build_folder/$dest_folder_name"
+set build_dest_icon_folder "$build_folder/$dest_folder_name/icons"
+set release_folder (pwd)"/dist/streamdeck-iconpack-system-uicons/$dest_folder_name"
+set src_folder (pwd)"/src/$sdip_slug"
+set tmp_folder (pwd)"/tmp/$sdip_slug"
+
+echo "Preparing to build Stream Deck icon pack \"$sdip_name\""
 echo
 
-if not ask_for_confirmation --question "Version $iconpack_version, correct?"
-  set iconpack_version (math $iconpack_version + 0.1)
-  if ask_for_confirmation --question "Bump version to $iconpack_version?"
-    echo "- Bumping version to $iconpack_version"
-    echo $iconpack_version > $version_file
-    git commit -m "[CHG] Bumps version" $version_file
+if not ask_for_confirmation --question "Version $sdip_version, correct?"
+  set sdip_version (math $sdip_version + 0.1)
+  if ask_for_confirmation --question "Bump version to $sdip_version?"
+    echo "- Bumping version to $sdip_version"
+    echo $sdip_version > $sdip_version_file
+    git commit -m "[CHG] Bumps version" $sdip_version_file
   else
     echo "Exiting"
     exit 1
@@ -22,18 +31,9 @@ if not ask_for_confirmation --question "Version $iconpack_version, correct?"
 end
 
 
-# Setup folders
-set iconpack_folder_name (slugify "$iconpack_name")".sdIconPack"
-set build_folder (pwd)"/build"
-set src_folder (pwd)"/src"
-set tmp_folder (pwd)"/tmp"
-set target_folder "$build_folder/$iconpack_folder_name"
-set icon_folder "$target_folder/icons"
-set dist_folder (pwd)"/dist/streamdeck-iconpack-system-uicons/$iconpack_folder_name"
-
 echo "- Setting up folders"
 rm -rf $build_folder $tmp_folder $src_folder >/dev/null 2>&1
-mkdir -p $target_folder $icon_folder $tmp_folder $src_folder
+mkdir -p $build_dest_folder $build_dest_icon_folder $tmp_folder $src_folder
 
 
 echo "- Downloading icons from https://systemuicons.com/"
@@ -43,8 +43,8 @@ unzip -jq $zip_file -d $src_folder
 
 
 echo "- Writing SVG build files"
-cp (ls $src_folder/*.svg) $icon_folder/
-sd '"currentColor"' '"#fff"' (ls $icon_folder/*.svg)
+cp (ls $src_folder/*.svg) $build_dest_icon_folder/
+sd '"currentColor"' '"#fff"' (ls $build_dest_icon_folder/*.svg)
 
 set extra_icons_red $src_folder/bell_disabled.svg \
                     $src_folder/camera_noflash_alt.svg \
@@ -65,9 +65,9 @@ set extra_icons_red $src_folder/bell_disabled.svg \
                     $src_folder/wifi_none.svg
 for svg_file in $extra_icons_red
   set --local svg_red (string replace ".svg" "__red.svg" (basename "$svg_file"))
-  cp $svg_file $icon_folder/$svg_red
+  cp $svg_file $build_dest_icon_folder/$svg_red
 end
-sd '"currentColor"' '"#f00"' (ls "$icon_folder"/*__red.svg)
+sd '"currentColor"' '"#f00"' (ls "$build_dest_icon_folder"/*__red.svg)
 
 set extra_icons_green $src_folder/info_circle.svg \
                       $src_folder/home_check.svg \
@@ -77,13 +77,13 @@ set extra_icons_green $src_folder/info_circle.svg \
                       $src_folder/clipboard_check.svg
 for svg_file in $extra_icons_green
   set --local svg_green (string replace ".svg" "__green.svg" (basename "$svg_file"))
-  cp $svg_file $icon_folder/$svg_green
+  cp $svg_file $build_dest_icon_folder/$svg_green
 end
-sd '"currentColor"' '"#0f0"' (ls $icon_folder/*__green.svg)
+sd '"currentColor"' '"#0f0"' (ls $build_dest_icon_folder/*__green.svg)
 
 
 echo "- Converting SVG build files to PNG"
-set icon_files (ls $icon_folder/*.svg)
+set icon_files (ls $build_dest_icon_folder/*.svg)
 for svg_file in $icon_files
   convert_svg_to_png --file $svg_file
 end
@@ -94,33 +94,33 @@ rm $icon_files
 
 
 echo "- Building icons list"
-png_list_to_json --folder $icon_folder > $target_folder/icons.json
+png_list_to_json --folder $build_dest_icon_folder > $build_dest_folder/icons.json
 
 
 echo "- Building manifest"
 echo "{
   \"Author\": \"Carlo Zottmann (icon pack), Corey Ginnivan (original System UIcons)\",
-  \"Description\": \"$iconpack_name\",
-  \"Name\": \"$iconpack_name\",
+  \"Description\": \"This is a free Stream Deck icon pack, based on the System UIcons iconset by Corey Ginnivan.\",
+  \"Name\": \"$sdip_name\",
   \"URL\": \"https://streamdeck-iconpacks.czm.io\",
-  \"Version\": \"$iconpack_version\",
+  \"Version\": \"$sdip_version\",
   \"Icon\": \"icons/create.png\",
   \"Tags\": \"\"
-}" > $target_folder/manifest.json
+}" > $build_dest_folder/manifest.json
 
 
-if ask_for_confirmation --question "Copy pack into SD IconPacks/ folder?  (This will overwrite an existing $iconpack_folder_name pack!)"
-  set sd_data_folder "$HOME/Library/Application Support/com.elgato.StreamDeck/IconPacks/$iconpack_folder_name"
+if ask_for_confirmation --question "Copy pack into SD IconPacks/ folder, possibly overwriting existing?"
+  set sd_data_folder "$HOME/Library/Application Support/com.elgato.StreamDeck/IconPacks/$dest_folder_name"
   echo "- Copying icon pack to $sd_data_folder"
   rm -rf $sd_data_folder
-  cp -R $target_folder $sd_data_folder
+  cp -R $build_dest_folder $sd_data_folder
 end
 
 if ask_for_confirmation --question "Copy pack into dist/ folder?"
-  echo "- Copying icon pack to $dist_folder"
-  rm -rf $dist_folder/* >/dev/null 2>&1
-  cp -R $target_folder/* $dist_folder/
-  cp $dist_folder/../LICENSE.md $dist_folder/
+  echo "- Copying icon pack to $release_folder"
+  rm -rf $release_folder/* >/dev/null 2>&1
+  cp -R $build_dest_folder/* $release_folder/
+  cp $release_folder/../LICENSE.md $release_folder/
 end
 
 # TODO: Ask to update website

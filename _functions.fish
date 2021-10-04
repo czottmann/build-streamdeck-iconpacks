16da401db1fda3980183a92a3aa81d451c090678
Carlo Zottmann <carlo@zottmann.org>
2021-10-04 14:58:49 +0200

    [CHG] Moves reusable code into separate functions

--- _functions.fish
+++ _functions.fish
@@ -16,6 +16,36 @@ function ask_for_confirmation
 end
 
 
+function confirm_current_version
+  set sdip_version_file "version.$sdip_slug.txt"
+  set sdip_version (cat $sdip_version_file | string trim)
+
+  if not ask_for_confirmation --question "Version $sdip_version, correct?"
+    set sdip_version (math $sdip_version + 0.1)
+    if ask_for_confirmation --question "Bump version to $sdip_version?"
+      echo "- Bumping version to $sdip_version"
+      echo $sdip_version > $sdip_version_file
+      git commit -m "[CHG] Bumps version" $sdip_version_file
+    else
+      echo "Exiting"
+      exit 1
+    end
+  end
+
+  echo -n $sdip_version
+end
+
+
+function convert_all_svg_files_to_png
+  set icon_files $argv
+
+  echo "- Converting" (count $argv) "SVG build files to PNG"
+  for svg_file in $icon_files
+    convert_svg_to_png --file $svg_file
+  end
+end
+
+
 function convert_svg_to_png
   argparse 'f/file=' -- $argv
   or return
@@ -29,15 +59,91 @@ function convert_svg_to_png
 end
 
 
-function png_list_to_json
-  argparse 'f/folder=' -- $argv
-  or return
+function create_icons_json_file
+  echo "- Creating icons.json file"
 
   test -d tmp/ || mkdir tmp/
   set --local tmp_file tmp/icons.csv
 
   echo "path" > $tmp_file
-  fd --base-directory $_flag_folder --extension png >> $tmp_file
+  fd --base-directory $build_dest_icon_folder --extension png >> $tmp_file
   csvtk csv2json $tmp_file \
-    | jq 'map({ path, name: (.path | sub(".png"; "") | gsub("_+"; " ")), tags: [] })'
+    | jq 'map({ path, name: (.path | sub(".png"; "") | gsub("_+"; " ")), tags: [] })' \
+    > $build_dest_folder/icons.json
+end
+
+
+function create_manifest_json_file
+  argparse 'a/author=' 'd/desc=' 'i/icon=' 't/tags=' -- $argv
+  or return
+
+  echo "- Creating manifest.json file"
+  echo "{
+          \"Author\": \"$_flag_author\",
+          \"Description\": \"$_flag_desc\",
+          \"Name\": \"$sdip_name\",
+          \"URL\": \"https://streamdeck-iconpacks.czm.io\",
+          \"Version\": \"$sdip_version\",
+          \"Icon\": \"$_flag_icon\",
+          \"Tags\": \"$_flag_tags\"
+        }" \
+    | jq \
+    > $build_dest_folder/manifest.json
+end
+
+
+function optional_copy_to_dist_folder
+  if ask_for_confirmation --question "Copy pack into dist/ folder?"
+    echo "- Copying icon pack to $release_folder"
+    rm -rf $release_folder/* >/dev/null 2>&1
+    cp -R $build_dest_folder/* $release_folder/
+    cp $release_folder/../LICENSE.md $release_folder/
+  end
+end
+
+
+function optional_copy_to_local_sd_folder
+  if ask_for_confirmation --question "Copy pack into SD IconPacks/ folder, possibly overwriting existing?"
+    set sd_data_folder "$HOME/Library/Application Support/com.elgato.StreamDeck/IconPacks/$dest_folder_name"
+    echo "- Copying icon pack to $sd_data_folder"
+    rm -rf $sd_data_folder
+    cp -R $build_dest_folder $sd_data_folder
+  end
+end
+
+
+function remove_all_svg_build_files
+  echo "- Deleting SVG build files"
+  rm $argv
+end
+
+
+function remove_working_folders
+  echo "- Cleaning up working folders"
+  rm -rf $build_folder $tmp_folder $src_folder >/dev/null 2>&1
+end
+
+
+function say_hello
+  echo "Preparing to build Stream Deck icon pack \"$sdip_name\""
+  echo
+end
+
+
+function setup_folder_variables
+  set --global dest_folder_name "$sdip_slug.sdIconPack"
+
+  set --global build_folder (pwd)"/build/$sdip_slug"
+  set --global build_dest_folder "$build_folder/$dest_folder_name"
+  set --global build_dest_icon_folder "$build_folder/$dest_folder_name/icons"
+  set --global release_folder (pwd)"/dist/streamdeck-iconpack-$sdip_slug/$dest_folder_name"
+  set --global src_folder (pwd)"/src/$sdip_slug"
+  set --global tmp_folder (pwd)"/tmp/$sdip_slug"
+end
+
+
+function setup_working_folders
+  echo "- Setting up folders"
+  rm -rf $build_folder $tmp_folder $src_folder >/dev/null 2>&1
+  mkdir -p $build_dest_folder $build_dest_icon_folder $tmp_folder $src_folder
 end
--- build-system-uicons.fish
+++ build-system-uicons.fish
@@ -2,38 +2,13 @@
 
 source ./_functions.fish
 
-set sdip_name "System UIcons"
-set sdip_slug (slugify "$sdip_name")
-set dest_folder_name "$sdip_slug.sdIconPack"
-set sdip_version_file version.system-uicons.txt
-set sdip_version (cat $sdip_version_file | string trim)
+set sdip_name 'System UIcons'
+set sdip_slug 'system-uicons'
 
-set build_folder (pwd)"/build/$sdip_slug"
-set build_dest_folder "$build_folder/$dest_folder_name"
-set build_dest_icon_folder "$build_folder/$dest_folder_name/icons"
-set release_folder (pwd)"/dist/streamdeck-iconpack-system-uicons/$dest_folder_name"
-set src_folder (pwd)"/src/$sdip_slug"
-set tmp_folder (pwd)"/tmp/$sdip_slug"
-
-echo "Preparing to build Stream Deck icon pack \"$sdip_name\""
-echo
-
-if not ask_for_confirmation --question "Version $sdip_version, correct?"
-  set sdip_version (math $sdip_version + 0.1)
-  if ask_for_confirmation --question "Bump version to $sdip_version?"
-    echo "- Bumping version to $sdip_version"
-    echo $sdip_version > $sdip_version_file
-    git commit -m "[CHG] Bumps version" $sdip_version_file
-  else
-    echo "Exiting"
-    exit 1
-  end
-end
-
-
-echo "- Setting up folders"
-rm -rf $build_folder $tmp_folder $src_folder >/dev/null 2>&1
-mkdir -p $build_dest_folder $build_dest_icon_folder $tmp_folder $src_folder
+say_hello
+set sdip_version (confirm_current_version)
+setup_folder_variables
+setup_working_folders
 
 
 echo "- Downloading icons from https://systemuicons.com/"
@@ -82,50 +57,20 @@ end
 sd '"currentColor"' '"#0f0"' (ls $build_dest_icon_folder/*__green.svg)
 
 
-echo "- Converting SVG build files to PNG"
 set icon_files (ls $build_dest_icon_folder/*.svg)
-for svg_file in $icon_files
-  convert_svg_to_png --file $svg_file
-end
+convert_all_svg_files_to_png $icon_files
+remove_all_svg_build_files $icon_files
 
+create_icons_json_file
+create_manifest_json_file \
+  --author "Carlo Zottmann (icon pack), Corey Ginnivan (original System UIcons)" \
+  --desc "This is a free Stream Deck icon pack, based on the System UIcons iconset by Corey Ginnivan." \
+  --icon 'icons/create.png' \
+  --tags ''
 
-echo "- Deleting SVG build files"
-rm $icon_files
-
-
-echo "- Building icons list"
-png_list_to_json --folder $build_dest_icon_folder > $build_dest_folder/icons.json
-
-
-echo "- Building manifest"
-echo "{
-  \"Author\": \"Carlo Zottmann (icon pack), Corey Ginnivan (original System UIcons)\",
-  \"Description\": \"This is a free Stream Deck icon pack, based on the System UIcons iconset by Corey Ginnivan.\",
-  \"Name\": \"$sdip_name\",
-  \"URL\": \"https://streamdeck-iconpacks.czm.io\",
-  \"Version\": \"$sdip_version\",
-  \"Icon\": \"icons/create.png\",
-  \"Tags\": \"\"
-}" > $build_dest_folder/manifest.json
-
-
-if ask_for_confirmation --question "Copy pack into SD IconPacks/ folder, possibly overwriting existing?"
-  set sd_data_folder "$HOME/Library/Application Support/com.elgato.StreamDeck/IconPacks/$dest_folder_name"
-  echo "- Copying icon pack to $sd_data_folder"
-  rm -rf $sd_data_folder
-  cp -R $build_dest_folder $sd_data_folder
-end
-
-if ask_for_confirmation --question "Copy pack into dist/ folder?"
-  echo "- Copying icon pack to $release_folder"
-  rm -rf $release_folder/* >/dev/null 2>&1
-  cp -R $build_dest_folder/* $release_folder/
-  cp $release_folder/../LICENSE.md $release_folder/
-end
-
+optional_copy_to_local_sd_folder
+optional_copy_to_dist_folder
 # TODO: Ask to update website
-
-echo "- Cleaning up working folders"
-rm -rf $build_folder $tmp_folder $src_folder >/dev/null 2>&1
+remove_working_folders
 
 echo "- Done!"
